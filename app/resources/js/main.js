1a609dd7ef621e0a2c79d175520b5da34dd2c17e
Danielle DelBrocco <dldelbrocco@gmail.com>
2018-10-27 10:32:40 -0400

    completed tasks list and enter to add

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -2,13 +2,39 @@ var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xli
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
 document.getElementById('add').addEventListener('click', function() {
-    
     var value = document.getElementById('item').value;    
     if (value) {
         addItemTodo(value);
         document.getElementById('item').value = '';
     }
 });
+
+document.getElementById('item').addEventListener('keyup', function(e) {
+    var value = document.getElementById('item').value;
+    e.preventDefault();
+    if(e.keyCode === 13){
+        addItemTodo(value);
+        document.getElementById('item').value = '';
+    }
+});
+
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+
+    parent.removeChild(item);
+}
+
+function completeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var parentId = parent.id;
+
+    var target = (parentId === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+    
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
+}
 
 function addItemTodo(text){
     var list = document.getElementById('todo')
@@ -23,9 +49,13 @@ function addItemTodo(text){
     remove.classList.add('remove');
     remove.innerHTML= removeSVG;
 
+    remove.addEventListener('click', removeItem);
+
     var add = document.createElement('button');
     add.classList.add('complete');
     add.innerHTML = completeSVG;
+
+    add.addEventListener('click', completeItem);
 
     buttons.appendChild(remove);
     buttons.appendChild(add);
